f2040564ae4c3e751212eef469823c4141cf9d7c
Duong Duc Quan <quanduongduc@gmail.com>
2023-12-19 21:30:04 +0700

    update course_review seeders

---
+++ database/seeders/CourseReviewSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Database\Seeder;
+use App\Models\CourseReview;
+use App\Models\User;
+use Illuminate\Support\Facades\File;
+
+class CourseReviewSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Assuming you have users in your database
+        $userIds = User::pluck('id')->toArray();
+
+        $reviewsData = json_decode(File::get(database_path('seeders/course_reviews.json')), true);
+
+        // Seed the reviews
+        foreach ($reviewsData as $review) {
+            $review['user_id'] = $this->getRandomUserId($userIds);
+            CourseReview::create($review);
+        }
+    }
+
+    /**
+     * Get a random user ID from the provided array of user IDs.
+     *
+     * @param array $userIds
+     * @return int
+     */
+    private function getRandomUserId(array $userIds)
+    {
+        return $userIds[array_rand($userIds)];
+    }
+}
